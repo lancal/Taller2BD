0e3a5b6b51f7dd40a5b9be36d291c9b3b1741c2c
Alan Calderon <ivda.calderon@gmail.com>
2016-05-09 22:52:29 -0300

    alan

--- Login.vb
+++ Login.vb
@@ -31,51 +31,78 @@
             MsgBox("Por favor ingrese todos los datos antes de continuar",, "Faltan datos")
         Else
 
+
+
+
             If (consulta.getLogin(Me.motor, tbuser.Text, tbpass.Text, "persona").Rows.Count > 0) Then
 
 
 
-                'verificar a que tipo de usuario corresponde en el sistema
-                If (consulta.getUsuarioPorRut(Me.motor, tbuser.Text, "administrador").Rows.Count > 0) Then
-                    Dim admin As New Administrador(Me.motor, Me)
-                    MsgBox("Bienvenido Administrador")
-                    admin.Show()
-                    Me.Dispose()
+                    'verificar a que tipo de usuario corresponde en el sistema
+                    If (consulta.getUsuarioPorRut(Me.motor, tbuser.Text, "administrador").Rows.Count > 0) Then
+                        Dim admin As New Administrador(Me.motor, Me)
+                        MsgBox("Bienvenido Administrador")
+                        admin.Show()
+                        Me.Dispose()
 
 
-                ElseIf (consulta.getUsuarioPorRut(Me.motor, tbuser.Text, "trabajador").Rows.Count > 0) Then
-                    If (consulta.getTipoTrabajador(motor, tbuser.Text.ToString(), "trabajador").Equals("Control de Vuelos")) Then
-                        Dim trabajador As New Trabajador(Me.motor, Me)
-                        MsgBox("Bienvenido Trabajador")
-                        trabajador.Show()
-                        Me.Dispose()
-                    Else
-                        'atencion al cliente
-                        Dim atencionAlcliente As New AtencionAlCliente(Me.motor, Me)
-                        MsgBox("Bienvenido Trabajador")
-                        atencionAlcliente.Show()
-                        Me.Dispose()
+                    ElseIf (consulta.getUsuarioPorRut(Me.motor, tbuser.Text, "trabajador").Rows.Count > 0) Then
+                        If (consulta.getTipoTrabajador(motor, tbuser.Text.ToString(), "trabajador").Equals("Control de Vuelos")) Then
+                            Dim trabajador As New Trabajador(Me.motor, Me)
+                            MsgBox("Bienvenido Trabajador")
+                            trabajador.Show()
+                            Me.Dispose()
+                        Else
+                            'atencion al cliente
+                            Dim atencionAlcliente As New AtencionAlCliente(Me.motor, Me)
+                            MsgBox("Bienvenido Trabajador")
+                            atencionAlcliente.Show()
+                            Me.Dispose()
+
+                        End If
 
                     End If
+
+
+
+            Else
+
+
+
+                    MsgBox("Usuario no se encuentra en el sistema")
 
                 End If
 
 
-                Else
-
-
-
-                MsgBox("Usuario no se encuentra en el sistema")
 
             End If
 
 
 
-        End If
-
-
-
     End Sub
+
+    Public Function RutDigito(ByVal Rut As Long) As String
+        Dim Digito As Integer
+        Dim Contador As Integer
+        Dim Multiplo As Integer
+        Dim Acumulador As Integer
+
+        Contador = 2
+        Acumulador = 0
+        While Rut <> 0
+            Multiplo = (Rut Mod 10) * Contador
+            Acumulador = Acumulador + Multiplo
+            Rut = Rut \ 10
+            Contador = Contador + 1
+            If Contador = 8 Then
+                Contador = 2
+            End If
+        End While
+        Digito = 11 - (Acumulador Mod 11)
+        RutDigito = CStr(Digito)
+        If Digito = 10 Then RutDigito = "K"
+        If Digito = 11 Then RutDigito = "0"
+    End Function
 
     Private Sub tbuser_TextChanged(sender As Object, e As EventArgs) Handles tbuser.TextChanged
 
